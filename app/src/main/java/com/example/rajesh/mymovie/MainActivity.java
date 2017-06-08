d307f1977fb09d42e15cf5587ae49e44807d431e
siva krishna <1234.shiva.krishna@gmail.com>
2017-06-08 13:43:12 -0400

    changed1

--- app/src/main/java/com/example/rajesh/mymovie/MainActivity.java
+++ app/src/main/java/com/example/rajesh/mymovie/MainActivity.java
@@ -1,14 +1,15 @@
 package com.example.rajesh.mymovie;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //changed
+
     }
 }
